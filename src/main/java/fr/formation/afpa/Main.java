cd9fa815e3c7bbe55179c930d8a11bce0da9b1c1
memaerten <me.maerten@gmail.com>
2021-05-07 09:57:06 +0200

    essai commit

---
+++ src/main/java/fr/formation/afpa/Main.java
@@ -0,0 +1,12 @@
+package fr.formation.afpa;
+
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+
+public class Main {
+	public static final Log log = LogFactory.getLog(Main.class);
+	
+	public static void main(String[] args) {
+		log.info("blablabla");
+	}
+}
