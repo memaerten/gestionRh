b01443e43d727f46ba7313797752524c88774477
memaerten <me.maerten@gmail.com>
2021-05-12 11:17:38 +0200

    RESPONSIVE NAVBAR and i18n

---
+++ src/main/java/fr/formation/afpa/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package fr.formation.afpa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+
+//	@Autowired
+//	private AuthentificationPoint auth;
+//	
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("root").password("123456");
+//		
+//	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/securityNone").permitAll().anyRequest().authenticated().and().httpBasic().authenticationEntryPoint(auth);
+//		
+//		//http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
+//	}
+//	
+//	
+}
